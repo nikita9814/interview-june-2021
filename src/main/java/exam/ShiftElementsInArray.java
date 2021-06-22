674317cf5e70e2b150ad989b3259594747a0f199
nikita9814 <68469295+nikita9814@users.noreply.github.com>
2021-06-22 12:43:41 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -2,8 +2,25 @@ package exam;
 
 public class ShiftElementsInArray {
 
-  public void shift(int[] arr) {
+  public void shift(int sum,intvals[] ) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+  
+        if (sum < 0) {
+            return 0;
+        }
+        if (vals == null || vals.length == 0) {
+            return 0;
+        }
+
+        int dp[] = new int[sum + 1];
+        dp[0] = 1;
+        for (int i = 0; i < vals.length; ++i) {
+            for (int j = vals[i]; j <= sum; ++j) {
+                dp[j] += dp[j - vals[i]];
+            }
+        }
+        return dp[sum];
+    }
 
   }
 
